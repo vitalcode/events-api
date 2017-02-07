f5a5ea1da6592b2443f7202979aed3c1045b4841
Vitaliy Kuznetsov <vitaliy.kuznetsov@yahoo.co.uk>
2017-02-07 00:11:11 +0000

    Update docker file

--- project/Build.scala
+++ project/Build.scala
@@ -1,8 +1,6 @@
 import sbt._
 
 object Build extends Build {
-
-  lazy val root = Project("root", file(".")) dependsOn eventsModel
-  lazy val eventsModel =
-    RootProject(uri("https://github.com/vitalcode/events-model.git"))
+  lazy val root = Project("root", file(".")).dependsOn(eventsModel)
+  lazy val eventsModel = RootProject(uri("https://github.com/vitalcode/events-model.git"))
 }
